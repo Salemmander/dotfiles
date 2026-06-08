754f02293ba3dd0a95acf3e1a5279abb6568b418
Salem Nassar <salemnassar5@gmail.com>
2026-06-08 17:08:20 -0500

    fix codex sandbox

--- common/.config/nvim/lua/plugins/snacks-image.lua
+++ common/.config/nvim/lua/plugins/snacks-image.lua
@@ -1,6 +1,8 @@
 return {
   "folke/snacks.nvim",
   opts = {
-    image = { enabled = true },
+    -- snacks.image uses the Kitty Graphics Protocol. In Ghostty inside tmux it
+    -- can leak PNG bytes into Neovim buffers when passthrough is enabled.
+    image = { enabled = vim.env.TMUX == nil },
   },
 }
