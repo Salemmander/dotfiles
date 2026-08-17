30d0f6044e7d81287ff958e70d6a98b40a45c3f4
Salem Nassar <salemnassar5@gmail.com>
2026-08-17 18:07:19 -0500

    cleanup

--- common/.config/nvim/lua/config/options.lua
+++ common/.config/nvim/lua/config/options.lua
@@ -1,6 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- Machine-local Omarchy helper. Absent on other machines; do not vendor it here.
+local ok, remote_clipboard = pcall(require, "config.remote_clipboard")
+if ok then
+  remote_clipboard.setup()
+end
 vim.opt.relativenumber = true
 vim.opt.smoothscroll = true
 vim.opt.autoread = true
